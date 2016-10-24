ff47d99955e90f71be826f5dfa13b67b76330b5a
Tom McClelland <tom@mcclelland.me>
2016-10-24 14:10:19 -0400

    explorer mode complete

--- examples.js
+++ examples.js
@@ -1,1 +1,21 @@
 // Your test code examples go here. Make sure you console.log() results.
+// one(): Find one matching element using any CSS selector
+console.log(md.one('.active'))
+// all(): Find all matching elements using any CSS selector
+console.log(md.all('tr'))
+// remove(): Remove an element
+md.remove('table')
+// addClass(): Add a CSS class to an element
+md.addClass('.sub-header', 'bg-success')
+// removeClass(): Remove a CSS class from an element
+md.removeClass('.sub-header','bg-success')
+// hasClass(): Return true if an element contains a CSS class in it's CSS attribute (see classList documentation)
+console.log(md.hasClass('h1', 'page-header'))
+// getAttr(): Get an HTML attribute from an element
+console.log(md.getAttr('img:first-child', 'width'))
+// setAttr(): Set an HTML attribute on an element
+md.setAttr('img:first-child', 'alt', 'my first pic')
+// setHTML(): Set the innerHTML of an element
+md.setHTML('h1', 'Tom\'s Cool Header')
+// getHTML(): Get the innerHTML of an element
+console.log(md.getHTML('h1'))
--- modom.js
+++ modom.js
@@ -3,7 +3,56 @@
 (function() {
     'use strict'
 
+    var one = function(selector) {
+        return document.querySelector(selector)
+    }
+
+    var all = function (selector) {
+        return document.querySelectorAll(selector)
+    }
+
+    var remove = function (element) {
+        one(element).remove()
+    }
+
+    var addClass = function (element, cssClass) {
+        one(element).classList.add(cssClass)
+    }
+
+    var removeClass = function (element, cssClass) {
+        one(element).classList.remove(cssClass)
+    }
+
+    var hasClass = function (element, cssClass) {
+        return one(element).classList.contains(cssClass)
+    }
+
+    var getAttr = function (element, attr) {
+        return one(element).getAttribute(attr)
+    }
+
+    var setAttr = function (element, attr, value) {
+        one(element).setAttribute(attr, value)
+    }
+
+    var setHTML = function (element, html) {
+        one(element).innerHTML = html;
+    }
+
+    var getHTML = function (element) {
+        return one(element).innerHTML
+    }
+
     window.md = {
-        
+        one,
+        all,
+        remove,
+        addClass,
+        removeClass,
+        hasClass,
+        getAttr,
+        setAttr,
+        setHTML,
+        getHTML
     }
 }())
